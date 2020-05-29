29ba126f62aaec9f9799187e53ee12251c633baf
essarrgee <55785294+essarrgee@users.noreply.github.com>
2020-05-29 16:20:05 -0700

    Create ronaldinho.js

---
+++ ronaldinho.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+function CheckRonaldinho(string) {
+	var re = new RegExp('(r+)(o+)(n*)ald(i*)(n*)(h*)(i*)(o+)');
+	var isRonaldinho = re.test(string);
+	return isRonaldinho;
+}
+
+client.on('ready', ()=>{
+	client.user.setActivity('with ur mom lol');
+})
+
+client.on('message', (message) => {
+	if (message.author != client.user) {
+		var isRonaldinho = CheckRonaldinho(message);
+		if (isRonaldinho) {
+			message.channel.send("Hahahaha... RONALDINHO SOCCERRR");
+		}
+	}
+})
+
+//client.login("BOT ID GOES IN HERE");
